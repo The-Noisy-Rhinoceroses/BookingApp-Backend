09a292909f4a4e13fb4dfa2eb2e67af1277add6a
Man Jun (John) Hui <mjhui90@gmail.com>
2018-09-17 13:52:20 -0400

    feat(db): Add Schema and Validation for Customers (#12)
    * feat(db): Add Schema and Validation for Customers
    
    * feat(db): Add Schema and Validation for Customers

---
+++ db/customers.js
@@ -0,0 +1,28 @@
+db.createCollection('customers', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['firstName', 'lastName', 'phoneNumber', 'email'],
+      properties: {
+        firstName: {
+          bsonType: 'string',
+          description: 'must be a string and is required'
+        },
+        lastName: {
+          bsonType: 'string',
+          description: 'must be a string and is required'
+        },
+        phoneNumber: {
+          bsonType: 'string',
+          description: 'must be a string and is required'
+        },
+        email: {
+          bsonType: 'string',
+          pattern: '^.+@.+$',
+          description:
+            'must be a string and match the regular expression pattern'
+        }
+      }
+    }
+  }
+});
