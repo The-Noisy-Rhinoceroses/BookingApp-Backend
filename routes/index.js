df1ef6d221e587d7e8b4b18dbfba9a4deda0fb65
richardsunny2015 <richardsunny2015@gmail.com>
2018-09-17 15:17:44 -0400

    feat(customers): add customer routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const indexRouter = express.Router();
 const barberRouter = require('./barbers');
+const customerRouter = require('./customers');
 
-const apiRoutes = (indexRouter, barberRouter) => (db) => {
+const apiRoutes = (indexRouter, barberRouter, customerRouter) => (db) => {
   indexRouter.use('/barbers', barberRouter(db));
+  indexRouter.use('/customers', customerRouter(db));
   return indexRouter;
 }
 
-module.exports = apiRoutes(indexRouter, barberRouter);
+module.exports = apiRoutes(indexRouter, barberRouter, customerRouter);
