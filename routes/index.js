70e0a92f3c2ee2720e3b7188f563d6df462a0480
Man Jun (John) Hui <mjhui90@gmail.com>
2018-09-18 17:15:41 -0400

    feat(appointment.js):User can retrieve apt (#21)
    * feat(appointment.js):User can retrieve apt
    
    * fix(moment): moment in package.json

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const indexRouter = express.Router();
 const barberRouter = require('./barbers');
 const customerRouter = require('./customers');
+const appointmentRouter = require('./appointments');
 
-const apiRoutes = (indexRouter, barberRouter, customerRouter) => (db) => {
+const apiRoutes = (indexRouter, barberRouter, customerRouter, appointmentRouter) => (db) => {
   indexRouter.use('/barbers', barberRouter(db));
   indexRouter.use('/customers', customerRouter(db));
+  indexRouter.use('/appointments', appointmentRouter(db));
   return indexRouter;
 }
 
-module.exports = apiRoutes(indexRouter, barberRouter, customerRouter);
+module.exports = apiRoutes(indexRouter, barberRouter, customerRouter, appointmentRouter);
