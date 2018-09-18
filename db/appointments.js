e11be622f3c6ee40372dee41c1928305e4778ccd
richardsunny2015 <richardsunny2015@gmail.com>
2018-09-18 15:02:25 -0400

    feat(appointments): make schema for appointments

---
+++ db/appointments.js
@@ -0,0 +1,37 @@
+// schema
+db.createCollection('appointments', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['customerId', 'barberId', 'date'],
+      properties: {
+        customerId: {
+          bsonType: 'string',
+          description: 'must be a string and is required'
+        },
+        barberId: {
+          bsonType: 'string',
+          description: 'must be a string and is required'
+        },
+        date: {
+          bsonType: 'date',
+          description: 'must be a date and is required'
+        }
+      }
+    }
+  }
+});
+
+// example
+db.appointments.insertOne({
+  customerId: '1',
+  barberId: '1',
+  date: new Date('September 20, 2018 17:30:00')
+});
+
+db.appointments.find({
+  date: {
+    $gte: new Date('September 20, 2018 00:00:00'),
+    $lt: new Date('September 21, 2018 00:00:00')
+  }
+});
