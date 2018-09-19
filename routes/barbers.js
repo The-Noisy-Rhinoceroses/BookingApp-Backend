4faf47f0553fd6cec5346131b702b3b70e1749c7
Allan James Lapid <ajLapid718@gmail.com>
2018-09-19 14:27:35 -0400

    feat(barbers): Create POST route for a new barber

--- routes/barbers.js
+++ routes/barbers.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET barbers listing. */
 const barberRoutes = (router) => (db) => {
   router.get('/', function(req, res, next) {
-    db.collection('Barbers')
+    db.collection('barbers')
     .find()
     .toArray()
-    .then(barbers => res.json(barbers))
+    .then(barbers => res.status(200).json(barbers))
     .catch(next);
+  });
+
+  router.post('/', (req, res, next) => {
+    const { name } = req.body;
+    db.collection('barbers')
+      .insertOne({ name })
+      .then(newBarber => res.status(201).json(newBarber))
+      .catch(next);
   });
 
   return router;
