354f8af6dbbe7fb4e0e48a9bc32381b440b90cca
Allan James Lapid <ajLapid718@gmail.com>
2018-09-19 21:39:20 -0400

    refactor(app): pass in useNewUrlParser to connect function

--- app.js
+++ app.js
@@ -26,15 +26,17 @@ const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${
 // Database Name
 const dbName = process.env.DATABASE_NAME;
 
-// Use connect method to connect to the server
-MongoClient.connect(
-  url,
-  function(err, client) {
-    assert.equal(null, err);
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    app.use('/api', indexRouter(db));
-  }
-);
+// The first parameter will contain the Error object if an error occurred, or null otherwise
+// The second parameter will contain the initialized database object, or null if an error occurred
+const initializeDb = function(err, client) {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+
+  const db = client.db(dbName);
+  app.use('/api', indexRouter(db));
+};
+
+// Connect the instance of MongoClient to MongoDB via the connection URL
+MongoClient.connect(url, { useNewUrlParser: true }, initializeDb);
 
 module.exports = app;
