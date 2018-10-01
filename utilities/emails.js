34002c0ba9bcc8df239a9a9afbb5ef848ecdbd12
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-01 17:01:00 -0400

    feat(emails): Send calendar event

--- utilities/emails.js
+++ utilities/emails.js
@@ -1,5 +1,7 @@
 const nodemailer = require('nodemailer');
 const moment = require('moment');
+const ical = require('ical-generator');
+const cal = ical({domain: 'gmail.com'});
 
 // Provide details about email address, password, and e-mailing platform;
 const transporter = nodemailer.createTransport({
@@ -17,11 +19,34 @@ const getResponse = (error, info) => {
 
 // Fill out the content of the email and send it to the customer;
 const sendEmail = (customer, barber) => {
+  const event = cal.createEvent({
+    start: moment(customer.appointmentDate),
+    end: moment(customer.appointmentDate).add(30, 'minutes'),
+    organizer: 'Rezi <bookingapplication718@gmail.com>',
+    summary: 'SUMMARY',
+    description: 'DESCRIPTION'
+  });
+  const attendee = event.createAttendee({
+    email: customer.email,
+    name: `${customer.firstName} ${customer.lastName}`,
+    rsvp: 'true',
+    status: 'TENTATIVE'
+  });
   const mailOptions = {
     from: process.env.BOOKING_APP_EMAIL_ACCOUNT,
     to: customer.email,
     subject: 'Haircut Appointment Confirmation From Rezi!',
-    text: `Hello, ${customer.firstName} ${customer.lastName}!\n\nYou are receiving this e-mail as a friendly reminder that you set up an appointment on Rezi for ${moment(customer.appointmentDate).format("dddd, MMMM Do YYYY, h:mm a")} with our barber ${barber.barberFirstName} ${barber.barberLastName}!\n\nBest, The Team at Rezi`
+    text: `Hello, ${customer.firstName} ${
+      customer.lastName
+    }!\n\nYou are receiving this e-mail as a friendly reminder that you set up an appointment on Rezi for ${moment(
+      customer.appointmentDate
+    ).format('dddd, MMMM Do YYYY, h:mm a')} with our barber ${
+      barber.barberFirstName
+    } ${barber.barberLastName}!\n\nBest, The Team at Rezi`,
+    icalEvent: {
+      content: cal.toString(),
+      method: 'request'
+    }
   };
 
   transporter.sendMail(mailOptions, getResponse);
