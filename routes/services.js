4fda2b81a1191dd572d012add228c65a2654bd71
Allan James Lapid <ajLapid718@gmail.com>
2018-11-01 20:21:43 -0400

    YEAHUGH

--- routes/services.js
+++ routes/services.js
@@ -1,16 +1,57 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 const servicesRouter = db => {
-    router.get('/', (req, res, next) => {
-      db.collection('services')
-        .find()
-        .toArray()
-        .then(services => res.json(services))
-        .catch(next);
-    });
+  router.get('/', (req, res, next) => {
+    db.collection('services')
+      .find()
+      .toArray()
+      .then(services => res.json(services))
+      .catch(next);
+  });
 
-    return router;
+  router.post('/pay', (req, res, next) => {
+    const { amount } = req.body
+    const dataParameter = {
+      amount_money: {
+        amount,
+        currency_code: 'USD'
+      },
+      callback_url: process.env.SQUARE_CALLBACK_URL,
+      client_id: process.env.SANDBOX_APP_ID,
+      version: '1.3',
+      notes: 'notes for the transaction',
+      options: {
+        supported_tender_types: [
+          'CREDIT_CARD',
+          'CASH',
+          'OTHER',
+          'SQUARE_GIFT_CARD',
+          'CARD_ON_FILE'
+        ]
+      }
+    };
+
+    const url = `square-commerce-v1://payment/create?data=${JSON.stringify(dataParameter)}`;
+
+    axios
+      .get(url)
+      .then(res => res.data)
+      .then(whatIsThis => console.log(whatIsThis))
+      .catch(err => console.log(err))
+  });
+
+  router.post('/success', (req, res, next) => {
+    console.log(req.body);
+    console.log("POST!");
+  });
+
+  router.get('/success', (req, res, next) => {
+    console.log("GET LINE 51!");
+  });
+
+  return router;
 };
 
 module.exports = servicesRouter;
